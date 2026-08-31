ef88c7aa98a098ca21d0de74cc13e2709f780a49
Saimon Alam <saimonalam@Saimons-MacBook-Pro.local>
2026-08-31 14:58:05 +0600

    feat(user-model): Add user model and migration

--- migrations/env.py
+++ migrations/env.py
@@ -12,6 +12,7 @@ from app.documents.models.document import Document
 from app.documents.models.document_file import DocumentFile
 from app.documents.models.document_content import DocumentContent
 from app.documents.models.document_chunk import DocumentChunk
+from app.users.models.user import User
 
 config = context.config
 
