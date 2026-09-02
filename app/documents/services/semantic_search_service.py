3cfb359b292d4cc70a99e0df04da1be1dd6d104e
Saimon Alam <saimonalam@Saimons-MacBook-Pro.local>
2026-09-02 15:16:53 +0600

    Minor change

--- app/documents/services/semantic_search_service.py
+++ app/documents/services/semantic_search_service.py
@@ -25,11 +25,8 @@ class SemanticSearchService:
             raise RuntimeError("Unable to generate embedding for search query")
 
         # TODO: get user from request
-        try:
-            search_result = await self.chunk_repo.find_similar_chunks(
-                query_embedding=query_embedding[0],
-                user_id=UUID("77b03295-6eab-4d37-9429-2eeef614f278"),
-            )
-            return search_result
-        except Exception:
-            return []
+        search_result = await self.chunk_repo.find_similar_chunks(
+            query_embedding=query_embedding[0],
+            user_id=UUID("77b03295-6eab-4d37-9429-2eeef614f278"),
+        )
+        return search_result
